579ace8a15200fa72178d8aa76334855e734507f
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-10 13:28:01 -0300

    Conectando ao MongoDB, criando as camadas repository e service

--- src/main/java/com/kulandissa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/kulandissa/workshopmongo/resources/UserResource.java
@@ -1,20 +1,22 @@
 package com.kulandissa.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kulandissa.workshopmongo.domain.User;
+import com.kulandissa.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
 	// @RequestMapping(method=RequestMethod.GET) ou
 	@GetMapping
 	public ResponseEntity<List<User>> findAll()
@@ -23,11 +25,8 @@ public class UserResource {
 		 * ResponseEntity é usado de modo a facilitar o envio de dados para HTTP com todas as informações 
 		 * necessários no cabeçalho (o retorno até mesmo os possíveis erros
 		 * */
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
 		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = userService.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
